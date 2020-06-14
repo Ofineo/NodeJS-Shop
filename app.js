ad80ecb2626e51ce5552a1362e427b824458047a
Jordi <ofineo@gmail.com>
2020-06-14 09:40:57 +0100

    first routes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require("http");
 
-const routes = require("./routes");
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes);
+app.use('/add-product',(req,res,next)=>{
+    console.log('i\'m in another middleware');
+    res.send('<h1>Add products page</h1>');
+});
 
-server.listen(3000);
+app.use('/',(req,res,next)=>{
+    console.log('i\'m in another middleware');
+    res.send('Hello from Express');
+});
+
+app.listen(3000);
+
+
+
