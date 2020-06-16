543b5b5dddf972aa6285e2846ad78e439965f301
Jordi <ofineo@gmail.com>
2020-06-16 16:03:34 +0100

    separation of concerns MVC

--- app.js
+++ app.js
@@ -8,19 +8,18 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', 'views');
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //setup a path for static serving images, css, etc.
 app.use(express.static(path.join(__dirname,'public')));
 
 //base url is specified here as /admin so they will be considered only starting with that path 
-app.use('/admin',adminData.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle:'Error 404'});
-})
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,23 +1,11 @@
 //import express
-const express = require("express");
 const path = require("path");
+const express = require("express");
 
-//import from another file constants,varibles, functions etc.
-const adminData = require("./admin");
+const productController = require("../controllers/products");
 //create the router mini-app
 const router = express.Router();
 //use router instead of app
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productController.getProducts);
 //export the route so it can be imported in the main app
 module.exports = router;
-
