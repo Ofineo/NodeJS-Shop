bb3fab15756c5959f20bc0280591868cc0617ab1
Jordi <ofineo@gmail.com>
2020-06-16 20:10:43 +0100

    adding new templates and routes to show them

---
+++ controllers/shop.js
@@ -0,0 +1,45 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/index",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout",
+  });
+};
+
+exports.getProductDetail = (req, res, next) => {
+  res.render("shop/product-detail", {
+    pageTitle: "Product detail",
+    path: "shop/product-detail",
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,19 @@
 const path = require("path");
 const express = require("express");
 
-const productController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 //create the router mini-app
 const router = express.Router();
 //use router instead of app
-router.get("/", productController.getProducts);
+router.get("/", shopController.getIndex);
+
+router.get('/products', shopController.getProducts)
+
+router.get('/cart',shopController.getCart)
+
+router.get('/checkout',shopController.getCheckout)
+
+router.get("/product-detail", shopController.getProductDetail);
+
 //export the route so it can be imported in the main app
 module.exports = router;
