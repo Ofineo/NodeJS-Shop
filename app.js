3b4583ed08202dacc67d32ac21bda512007a6c89
Jordi <ofineo@gmail.com>
2020-06-20 16:12:43 +0100

    created a user for development

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -17,16 +19,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //setup a path for static serving images, css, etc.
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 //base url is specified here as /admin so they will be considered only starting with that path
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // Override tables
+  //   .sync({force:true})
   .sync()
   .then((result) => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Jordi", email: "basura@outlook.jp" });
+    }
+    return user;
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
