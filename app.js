9d2a2c18d73d99ae7ad7da848759e02fefcf8423
Jordi <ofineo@gmail.com>
2020-06-20 09:31:57 +0100

    connect to a SQL database

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const db = require("./util/database");
 
-app.set('view engine','ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //setup a path for static serving images, css, etc.
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-//base url is specified here as /admin so they will be considered only starting with that path 
-app.use('/admin',adminRoutes);
+//base url is specified here as /admin so they will be considered only starting with that path
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
