558afe062724516c1c89c51cdb594f39b8a6710e
Jordi <ofineo@gmail.com>
2020-06-23 14:07:51 +0100

    add a relation to products that relate to a user

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //setup a path for static serving images, css, etc.
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("5ef0bc131b7410ca8b84bcae")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+//middleware to create a static user and pass it in our requests
+app.use((req, res, next) => {
+  User.findById("5ef1fc89ed5bd34374237a15")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 //base url is specified here as /admin so they will be considered only starting with that path
 app.use("/admin", adminRoutes);
@@ -38,6 +39,16 @@ mongoose
     "mongodb+srv://nodeComplete:rYX7GHW1EobK0XFw@node-complete-5hx8z.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Jordi",
+          email: "basura@outlook.jp",
+          items: [],
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => {
